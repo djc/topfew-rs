06b2903b0ba05aaac594944502c29438e3d23a29
Dirkjan Ochtman <dirkjan@ochtman.nl>
2020-07-09 15:38:27 +0200

    Revert to memory mapping file chunks

--- src/chunks.rs
+++ src/chunks.rs
@@ -1,147 +1,66 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, Seek, SeekFrom};
-use std::path::PathBuf;
-
-use anyhow::Context;
-
-/// Create an Iterator over chunks of lines of a file.
-///
-/// The last line in a chunk potentially reads over the chunk byte boundary to find the line end.
-/// In the same way the first line searches the line end.
-pub fn chunks(path: PathBuf) -> anyhow::Result<Chunker<FileSource>> {
-    let size = File::open(&path)?.metadata()?.len();
-    Ok(Chunker::new(FileSource { path }, size, size / num_cpus::get() as u64))
-}
-
-pub struct Chunker<S: Source> {
-    source: S,
-    position: u64,
-    count: usize,
-    chunks: usize,
+pub struct Chunk<'a> {
     chunk_size: u64,
-    size: u64,
+    state: State<'a>,
 }
 
-impl<S: Source> Chunker<S> {
-    fn new(source: S, size: u64, chunk_size: u64) -> Self {
-        let chunks = if chunk_size == 0 {
-            0
-        } else {
-            size / chunk_size + 1.min(size % chunk_size)
-        } as usize;
-
-        Chunker {
-            source,
-            position: 0,
-            count: 0,
-            chunks,
+impl<'a> Chunk<'a> {
+    pub fn new(bytes: &'a [u8], first: bool, chunk_size: u64) -> Self {
+        Self {
             chunk_size,
-            size,
+            state: State::Start { bytes, first },
         }
     }
 }
 
-impl<S: Source> Iterator for Chunker<S> {
-    type Item = Chunk<S::Item>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.count == self.chunks {
-            return None;
-        }
-
-        let start = (self.count as u64) * self.chunk_size;
-        self.count += 1;
-        let input = self.source.call().ok()?;
-        let (chunk, position) =
-            Chunk::new(input, self.chunk_size, self.position, start, self.size).ok()?;
-        self.position = position;
-        Some(chunk)
-    }
-}
-
-#[derive(Debug)]
-pub struct Chunk<C> {
-    lines: std::io::Lines<C>,
-    position: u64,
-    end: u64,
-}
-
-impl<C> Chunk<C> {
-    pub fn new(
-        mut input: C,
-        chunk_size: u64,
-        mut position: u64,
-        start: u64,
-        size: u64,
-    ) -> anyhow::Result<(Self, u64)>
-    where
-        C: Seek + BufRead,
-    {
-        let skip = if position > start {
-            true
-        } else if start != position {
-            input.seek(SeekFrom::Start(start - 1))?;
-            let mut buf = [0 as u8; 1];
-            if let Ok(1) = input.read(&mut buf) {
-                buf[0] != b'\n'
-            } else {
-                false
+impl<'a> Iterator for Chunk<'a> {
+    type Item = &'a str;
+    fn next(&mut self) -> Option<&'a str> {
+        let (lines, pos) = match self.state {
+            State::Start { mut bytes, first } => {
+                let mut position = 0;
+                if !first {
+                    while bytes[0] != b'\n' {
+                        bytes = &bytes[1..];
+                        position += 1;
+                    }
+                    bytes = &bytes[1..];
+                    position += 1;
+                }
+                let lines = std::str::from_utf8(bytes).unwrap().lines();
+                self.state = State::Running { lines, position };
+                match &mut self.state {
+                    State::Running { lines, position } => (lines, position),
+                    _ => unreachable!(),
+                }
             }
-        } else {
-            false
+            State::Running {
+                ref mut lines,
+                ref mut position,
+            } => (lines, position),
+            State::End => return None,
         };
 
-        input.seek(SeekFrom::Start(start))?;
-        position = if skip {
-            let mut skip_leader = String::new();
-            let _ = input.read_line(&mut skip_leader)?;
-            start + skip_leader.len() as u64
-        } else {
-            start
-        };
-        let lines = input.lines();
-        let c = Self {
-            lines,
-            position,
-            end: size.min(start + chunk_size),
-        };
-        Ok((c, position))
-    }
-}
-
-impl<T> Iterator for Chunk<T>
-where
-    T: Seek + BufRead,
-{
-    type Item = String;
-    fn next(&mut self) -> Option<String> {
-        if self.position >= self.end {
+        if *pos > self.chunk_size {
+            self.state = State::End;
             return None;
         }
-        let l = self.lines.next()?.ok()?;
-        self.position += l.len() as u64 + 1;
+
+        let l = lines.next()?;
+        *pos += l.len() as u64 + 1;
         Some(l)
     }
 }
 
-pub trait Source: Sized {
-    type Item: Seek + BufRead;
-
-    fn call(&self) -> anyhow::Result<Self::Item>;
-}
-
-pub struct FileSource {
-    path: PathBuf,
-}
-
-impl Source for FileSource {
-    type Item = BufReader<File>;
-
-    fn call(&self) -> anyhow::Result<Self::Item> {
-        File::open(&self.path)
-            .map(BufReader::new)
-            .with_context(|| "Failed")
-    }
+enum State<'a> {
+    Start {
+        bytes: &'a [u8],
+        first: bool,
+    },
+    Running {
+        lines: std::str::Lines<'a>,
+        position: u64,
+    },
+    End,
 }
 
 #[cfg(test)]
@@ -190,17 +109,5 @@ mod tests {
         quickcheck::QuickCheck::new()
             .max_tests(300)
             .quickcheck(test_split_buf as fn(_, _) -> TestResult);
-    }
-
-    struct MemorySource {
-        bytes: Vec<u8>,
-    }
-
-    impl Source for MemorySource {
-        type Item = Cursor<Vec<u8>>;
-
-        fn call(&self) -> anyhow::Result<Self::Item> {
-            Ok(Cursor::new(self.bytes.clone()))
-        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
+use std::fs::File;
 use std::path::PathBuf;
 
+use memmap::Mmap;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 mod chunks;
 mod counter;
 mod key_finder;
 
-pub use chunks::chunks;
+pub use chunks::Chunk;
 pub use counter::{Counter, KeyCount};
 pub use key_finder::KeyFinder;
 
@@ -15,7 +17,13 @@ pub fn top_few_from_stream(
     kf: &KeyFinder,
     num: usize,
 ) -> anyhow::Result<Vec<KeyCount>> {
-    let total = chunks(path)?
+    let file = File::open(&path)?;
+    let chunks = num_cpus::get();
+    let chunk_size = file.metadata()?.len() as usize / chunks;
+    let map = unsafe { Mmap::map(&file)? };
+
+    let total = (0..chunks)
+        .map(|i| Chunk::new(&map[i * chunk_size..], i == 0, chunk_size as u64))
         .collect::<Vec<_>>()
         .into_par_iter()
         .map(|reader| {
