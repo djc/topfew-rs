f1e331acf1de4b1256e8764043066017e2556667
Dirkjan Ochtman <dirkjan@ochtman.nl>
2024-04-18 10:14:06 +0200

    Use jemalloc for a ~21% speedup

--- src/bin/tf.rs
+++ src/bin/tf.rs
@@ -54,3 +54,10 @@ impl FromStr for FieldSpec {
         Ok(FieldSpec { indices })
     }
 }
+
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
